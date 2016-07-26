027dbb1f77372640fddc90c611f0dfaeed119a01
Makoto Nishimura <mac.nishimura@gmail.com>
2016-07-26 15:57:45 +0900

    AddressInputControl対応を追加

--- JpaddressPluspakSample/Form1.Designer.cs
+++ JpaddressPluspakSample/Form1.Designer.cs
@@ -28,36 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.gcPostalBarCode1 = new GrapeCity.Win.BarCode.GcPostalBarCode();
             this.gcJPAddress1 = new GrapeCity.Win.JPAddress.GcJPAddress();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.gcShortcut1 = new GrapeCity.Win.Editors.GcShortcut(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.gcPostal1 = new GrapeCity.Win.Editors.GcPostal(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.gcPostal1)).BeginInit();
             this.SuspendLayout();
             // 
             // gcPostalBarCode1
             // 
+            this.gcPostalBarCode1.AddressInputControl = this.textBox2;
             this.gcPostalBarCode1.BarSize = ((GrapeCity.Win.BarCode.ValueType.BarSize)(resources.GetObject("gcPostalBarCode1.BarSize")));
-            this.gcPostalBarCode1.Location = new System.Drawing.Point(13, 76);
+            this.gcPostalBarCode1.Location = new System.Drawing.Point(13, 147);
             this.gcPostalBarCode1.Name = "gcPostalBarCode1";
             this.gcPostalBarCode1.Size = new System.Drawing.Size(243, 11);
-            this.gcPostalBarCode1.ZipCodeInputControl = this.textBox1;
             // 
-            // textBox1
+            // textBox2
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 36);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 19);
-            this.textBox1.TabIndex = 1;
+            this.textBox2.Location = new System.Drawing.Point(13, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(243, 19);
+            this.textBox2.TabIndex = 4;
+            // 
+            // gcPostal1
+            // 
+            this.gcPostal1.Fields.ZipCodeSeparator.Text = "-";
+            this.gcPostal1.Location = new System.Drawing.Point(13, 35);
+            this.gcPostal1.Name = "gcPostal1";
+            this.gcShortcut1.SetShortcuts(this.gcPostal1, new GrapeCity.Win.Editors.ShortcutCollection(new System.Windows.Forms.Keys[] {
+                System.Windows.Forms.Keys.F2,
+                ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Return)))}, new object[] {
+                ((object)(this.gcPostal1)),
+                ((object)(this.gcPostal1))}, new string[] {
+                "ShortcutClear",
+                "ApplyRecommendedValue"}));
+            this.gcPostal1.Size = new System.Drawing.Size(120, 20);
+            this.gcPostal1.TabIndex = 5;
+            this.gcPostal1.ValueChanged += new System.EventHandler(this.gcPostal1_ValueChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.gcPostal1);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.gcPostalBarCode1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gcPostal1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,7 +89,9 @@
 
         private GrapeCity.Win.BarCode.GcPostalBarCode gcPostalBarCode1;
         private GrapeCity.Win.JPAddress.GcJPAddress gcJPAddress1;
-        private System.Windows.Forms.TextBox textBox1;
+        private GrapeCity.Win.Editors.GcShortcut gcShortcut1;
+        private System.Windows.Forms.TextBox textBox2;
+        private GrapeCity.Win.Editors.GcPostal gcPostal1;
     }
 }
 
--- JpaddressPluspakSample/Form1.cs
+++ JpaddressPluspakSample/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GrapeCity.Win.Editors;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,18 @@ namespace JpaddressPluspakSample
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void gcPostal1_ValueChanged(object sender, EventArgs e)
+        {
+            this.gcPostalBarCode1.ZipCode = this.gcPostal1.Value;
+
+            // GcPostalに入力された郵便番号から住所が取得できる場合はtextBox2を補完する
+            if (gcPostal1.Addresses != null && gcPostal1.Addresses.Count() > 0)
+            {
+                var address = gcPostal1.Addresses[0];
+                this.textBox2.Text = address.Prefecture + address.City + address.Town;
+            }
         }
     }
 }
